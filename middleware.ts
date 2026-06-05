2a692b24557304a442faa84db027b3404c05c132
DesBLMMO <trandunghy020906@gmail.com>
2026-06-05 16:41:46 +0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,12 @@
-// middleware.ts
 import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-export default withAuth();
+export default withAuth(
+  function middleware(req: NextRequest) {
+    return NextResponse.next();
+  }
+);
 
 export const config = {
   // Những trang bắt buộc phải có thẻ (Session Token) mới được vào
